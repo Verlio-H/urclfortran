04074994708f866116d43420c5de84d07740ce2c
Oliver Hines <olivermhines@gmail.com>
2025-02-17 02:51:23 -0800

    Add compiler CLI

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ A work in progress modern fortran compiler written entirely in modern fortran.
 Currently this compiler targets 16 bit URCL, but more backends will likely be added in the future.
 
 # Installing
-This compiler is intended to be installed using fpm (https://fpm.fortran-lang.org/install/index.html#install). From the root of the project, run `fpm install` to install the compiler. The compiler can then be executed by running `urclfortran`.
+This compiler is intended to be installed using fpm (https://fpm.fortran-lang.org/install/index.html#install). From the root of the project, run `fpm install --profile release` to install the compiler. The compiler can then be executed by running `urclfortran`.
 
 The compile.sh shell script is provided for developers and those who prefer a manual installation. This script is intended primarily for debugging purposes (to enable testing with ifort).
 
+# Usage
+urclfortran follows a rather typical syntax for command line arguments. Use `-o (filename)` to specify output file name. Use `-c` to disable linking.
+
+urclfortran relies on urcl-ld for linking urcl fles (https://github.com/Verlio-H/urcl-ld.git). The environment variable `LIBFORT_PATH` must be set to the global path of the libfortran directory for linking to work.
+
+urclfortran currently performs no preprocessing. In other words, the file extension is ignored and .F90 is equivalent to .f90.
+
 # Support
-Currently, this compiler supports a very very limited set of functionality. The code contained in input.f90 is effectively the extent of support.
+Currently, this compiler supports a very very limited set of functionality. The code contained in input.f90 is effectively the extent of support for fortran features.
--- app/main.f90
+++ app/main.f90
@@ -1,16 +1,55 @@
 program compiler
-  use include
-  use compile
-  implicit none
+    use include, only: read_file, carr
+    use compile, only: compiledata
+    implicit none
 
-  character(len=:), allocatable :: input
-  integer :: newunit
-  ! initialize data
-  input = read_file('input.f90')
-  ! call compile function
-  input = compiledata(input,'input.f90')
-  ! output result
-  open(newunit=newunit, file='output.urcl')
-  write(newunit, '(A)') input
-  ! cleanup
+    character(:), allocatable :: input, output
+    character(256) :: argument, libloc
+    character(:), allocatable :: ofname
+    type(carr) :: ifnames
+    logical :: link
+    integer :: newunit, argnum, argcnt
+
+    ! read arguments
+    link = .true.
+    argnum = 1
+    ofname = 'output.urcl'
+    argcnt = command_argument_count()
+    do while (argnum <= argcnt)
+        call get_command_argument(argnum, argument)
+        if (argument == '-o') then
+            argnum = argnum + 1
+            call get_command_argument(argnum, argument)
+            ofname = trim(argument)
+        else if (argument == '-c') then
+            link = .false.
+        else
+            call ifnames%append(trim(argument))
+        end if
+        argnum = argnum + 1
+    end do
+
+    if (.not.allocated(ifnames%array)) then
+        write(*, '(A)') 'error: no input files provided'
+        stop
+    end if
+
+    output = ''
+    do argnum = 1, ifnames%size - 1
+        ! initialize data
+        input = read_file(ifnames%array(argnum)%value)
+        
+        ! call compile function
+        output = output//compiledata(input, ifnames%array(argnum)%value)
+    end do
+
+    ! output result
+    open(newunit=newunit, file=ofname)
+    write(newunit, '(A)') output
+    close(newunit)
+
+    if (link) then
+        call get_environment_variable('LIBFORT_PATH',libloc)
+        call execute_command_line('urcl-ld -o '//ofname//' '//trim(libloc)//'/urcl16/main.urcl '//ofname)
+    end if
 end program
