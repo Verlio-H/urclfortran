4c38798d02b8a18e65c87a5ed2c12f94e3cf5fa7
Oliver Hines <olivermhines@gmail.com>
2025-02-17 11:21:24 -0800

    Clean compile file

--- src/compile.f90
+++ src/compile.f90
@@ -4,9 +4,9 @@ module compile
 
 contains
     function compiledata(input, fname, startline) result(result)
-        character(len=:), allocatable :: result
-        character(len=:), allocatable, intent(in) :: input
-        character(len=*), intent(in) :: fname
+        character(:), allocatable :: result
+        character(:), allocatable, intent(in) :: input
+        character(*), intent(in) :: fname
         integer(SMALL), allocatable, intent(in), optional :: startline
         
         type(ast) :: asted
@@ -18,29 +18,29 @@ contains
         integer :: i
         ! lexical analysis
         ! turns program text into array of tokens
-        call lex(input,fname,startline)
+        call lex(input, fname, startline)
 
         ! syntactic analysis
         ! turns array of tokens into a syntactic tree
-        asted = genast(lexed,fname)
-        !call print_ast(asted%nodes(1),asted,0)
+        asted = genast(lexed, fname)
+        ! call print_ast(asted%nodes(1),asted,0)
         ! semantic analysis
         ! generates module files
-        allocate(symbol_info(asted%nodes(1)%subnodes%size-1))
-        do i=1,asted%nodes(1)%subnodes%size-1
-            associate(type=>asted%nodes(asted%nodes(1)%subnodes%array(i))%type)
-                if (type==NODE_MODULE .or. type==NODE_PROGRAM) then
-                    call genmodfile(asted,asted%nodes(1)%subnodes%array(i),symbol_info(i))
+        allocate(symbol_info(asted%nodes(1)%subnodes%size - 1))
+        do i = 1, asted%nodes(1)%subnodes%size - 1
+            associate(type => asted%nodes(asted%nodes(1)%subnodes%array(i))%type)
+                if (type == NODE_MODULE .or. type == NODE_PROGRAM) then
+                    call genmodfile(asted, asted%nodes(1)%subnodes%array(i), symbol_info(i))
                 end if
             end associate
         end do
         
         ! ssa generation
         ! generates ssa code using a combination of the module files and the ast and automatically creates cfg
-        call gen_ir(asted,symbol_info,ssa,maxvar,varsizes)
+        call gen_ir(asted, symbol_info, ssa, maxvar, varsizes)
 
         if (allocated(ssa)) then
-            do i=1,size(ssa)
+            do i = 1, size(ssa)
                 !call ir_print(ssa(i)%ptr)
             end do
         end if
@@ -48,7 +48,7 @@ contains
         result = gen_asm(ssa,maxvar,varsizes)
 
         if (allocated(ssa)) then
-            do i=1,size(ssa)
+            do i = 1, size(ssa)
                 call ir_finalize(ssa(i)%ptr)
             end do
         end if
