92396e5525afff47e14295acf5427499aa552176
Oliver Hines <olivermhines@gmail.com>
2025-02-17 01:12:44 -0800

    Move to fpm

---
+++ app/main.f90
@@ -0,0 +1,16 @@
+program compiler
+  use include
+  use compile
+  implicit none
+
+  character(len=:), allocatable :: input
+  integer :: newunit
+  ! initialize data
+  input = read_file('input.f90')
+  ! call compile function
+  input = compiledata(input,'input.f90')
+  ! output result
+  open(newunit=newunit, file='output.urcl')
+  write(newunit, '(A)') input
+  ! cleanup
+end program
