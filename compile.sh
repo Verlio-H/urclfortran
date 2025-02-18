e513417ed20b6a2ebb1c0fe0b0d7d3b4182062b5
Oliver Hines <olivermhines@gmail.com>
2025-02-18 15:28:23 -0800

    Add flang and ifx to compile.sh

--- compile.sh
+++ compile.sh
@@ -2,12 +2,16 @@
 
 case $2 in
     "ifort") COMPILER="ifort";;
+    "ifx") COMPILER="ifx";;
+    "flang") COMPILER="flang-new";;
     *) COMPILER="gfortran";;
 esac
 case $1 in
     "debug") 
         case $2 in
             "ifort") ARGS="-O0 -g -gen-interface -warn all -traceback -check all -check bounds -check uninit -check -ftrapuv -debug all";;
+            "ifx") ARGS="-O0 -g -gen-interface -warn all -traceback -check all -check bounds -check uninit -check -ftrapuv -debug all";;
+            "flang") ARGS="-std=f2018 -O0";;
             *) ARGS="-std=f2018 -O0 -Wimplicit-interface -g -fcheck=all -fbacktrace -ffpe-trap=invalid,zero,overflow -finit-real=snan -finit-integer=-99999999";;
         esac
     ;;
